ae40109328de821a31e6610b19cdd2ab0db070fa
rashmikanta83 <swain.rashmi71@gmail.com>
2018-10-26 22:33:55 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,44 @@
+
+public class Employee {
+	
+	private String name;
+	private String designation;
+	private int salary;
+	
+	public Employee(String name,String designation,int salary){
+		this.name=name;
+		this.designation=designation;
+		this.salary=salary;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String toString(){
+		return name+"----"+designation+"----"+salary;
+
+	}
+	
+}
